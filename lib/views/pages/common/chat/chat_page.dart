2129f26c8ac4a42ca1c9addc29e6f269bb44ab44
Usama Azad <usamaazad2@gmail.com>
2022-10-07 21:22:20 +0500

    refactoring

--- lib/views/pages/common/chat/chat_page.dart
+++ lib/views/pages/common/chat/chat_page.dart
@@ -339,7 +339,7 @@ class _ChatPageState extends State<ChatPage> {
                         userId: ActiveUser.instance.user!.userId!,
                         paymentAmount: double.tryParse(priceController.text),
                         message:
-                            "Title: ${widget.assignment!.subject} Charges: \$${priceController.text} \nDescription: ${descriptionController.text}",
+                            "Title: ${widget.assignment!.subject} \nCharges: \$${priceController.text} \nDescription: ${descriptionController.text}",
                       ),
                     );
                     _messageController.clear();
